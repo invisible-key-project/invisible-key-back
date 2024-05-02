cc54f993aafa6385904f1bfa0d2d6982c7a7782b
eldeoddt <eldeoddt@gmail.com>
2024-05-02 18:05:00 +0900

    [ADD] views.py 수정 #2

--- embed/urls.py
+++ embed/urls.py
@@ -5,5 +5,5 @@ app_name = 'embed'
 
 urlpatterns = [
     path('embed/qr_data/', views.receive_and_process_qrdata, name='qrdata'),
-    path('embed/watermark_img/', views.receive_original_image, name='watermarked_img'),
+    path('embed/watermark_img/', views.receive_original_image, name='watermark_img'),
 ]
